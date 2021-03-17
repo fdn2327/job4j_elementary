ab6e600c8d123802138442044fd6f004b45e4549
fdn2327 <fdn2327@gmail.com>
2021-03-17 22:15:18 +0300

    Загрузка Info.java на github. [#498848]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Roman Denisiuk");
+        System.out.println("26.12.1983");
+    }
+}
