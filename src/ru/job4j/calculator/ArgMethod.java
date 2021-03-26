152da5032a8bcdb1436e53abd1745bc2c2238c0c
fdn2327 <fdn2327@gmail.com>
2021-03-26 21:53:18 +0300

    Задача 4.1

--- src/ru/job4j/calculator/ArgMethod.java
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -3,12 +3,19 @@ package ru.job4j.calculator;
 public class ArgMethod {
 
     public static void hello(String name) {
-        System.out.println("Hello, " + name);
+        System.out.println("Hello, you are " + name);
     }
 
     public static void main(String[] args) {
         String name = "Petr Arsentev";
-        int age = 37;
-        ArgMethod.hello(name);
+        String age = "33";
+
+        ArgMethod.hello(" ");
+
+        ArgMethod.hello(name); ArgMethod.hello(age);
+
+        ArgMethod.hello(age);
+
+        ArgMethod.hello(name); ArgMethod.hello(name); ArgMethod.hello(name);
     }
 }
