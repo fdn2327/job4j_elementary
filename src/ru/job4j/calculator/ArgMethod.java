296f297fe6e0f3f0d22fd899fda45d922b58d2bc
Roman <fdn2327@gmail.com>
2021-03-26 16:29:45 +0300

    Исправление кода

--- src/ru/job4j/calculator/ArgMethod.java
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -7,7 +7,8 @@ public class ArgMethod {
     }
 
     public static void main(String[] args) {
-        String name = "Roman Denisiuk";
+        String name = "Petr Arsentev";
+        int age = 37;
         ArgMethod.hello(name);
     }
 }
