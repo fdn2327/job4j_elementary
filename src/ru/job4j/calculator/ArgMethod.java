2a29c2978ba3d6faa26624018ab64b629d1a80b8
Roman <fdn2327@gmail.com>
2021-03-26 16:18:50 +0300

    test2

--- src/ru/job4j/calculator/ArgMethod.java
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -7,7 +7,7 @@ public class ArgMethod {
     }
 
     public static void main(String[] args) {
-        String name = "Petr Arsentev";
+        String name = "Roman Denisiuk";
         ArgMethod.hello(name);
     }
 }
