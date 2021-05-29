2fcd8e2087094447e51ccd9e9f0b3947969186f4
fdn2327 <fdn2327@gmail.com>
2021-05-29 21:39:53 +0300

    Новая версия расстояния между точками

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -2,10 +2,7 @@ package ru.job4j.condition;
 
 public class Point {
     public static double distance(int x1, int y1, int x2, int y2) {
-        double rsl1 = Math.pow((x2 - x1),2);
-        double rsl2 = Math.pow((y2 - x1),2);
-        double rsl3 = Math.sqrt(rsl1 + rsl2);
-        return rsl3;
+        return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - x1), 2));
     }
 
     public static void main(String[] args) {
