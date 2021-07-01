a29be3aef57ed170931d9bbdae06cdddbcbae7e4
fdn2327 <fdn2327@gmail.com>
2021-07-01 21:06:51 +0300

    Стороны прямоугольника

---
+++ src/ru/job4j/condition/SqArea.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+public class SqArea {
+    public static double square(double p, double k) {
+        return k*Math.pow((p/(2*k+2)),2);
+    }
+    public static void main(String[] args) {
+        double S = SqArea.square(6,2);
+        System.out.println(S);
+    }
+}
