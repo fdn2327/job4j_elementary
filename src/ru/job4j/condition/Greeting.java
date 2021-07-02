f603eac98288b8ee312457b0e09c69f6c5538f50
fdn2327 <fdn2327@gmail.com>
2021-07-02 22:35:17 +0300

    Задание 9.1

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+        String idea2 = idea + " But I am newbie.";
+        int year = 2021;
+        String space = " ";
+        String idea3 = idea2 + space + year;
+        System.out.println(idea3);
+    }
+}
