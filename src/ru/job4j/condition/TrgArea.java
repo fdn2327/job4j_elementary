c6ff13a7a2a332b3cf59a3f1f66c61dc555c9c79
fdn2327 <fdn2327@gmail.com>
2021-07-02 11:32:15 +0300

    Площадь треугольника по формуле Герона_исправлено_2.0

--- src/ru/job4j/condition/TrgArea.java
+++ src/ru/job4j/condition/TrgArea.java
@@ -2,13 +2,13 @@ package ru.job4j.condition;
 
 public class TrgArea {
 
-    public static double area(double a, double b, double c, double p) {
-        p = ((a+b+c)/2);
+    public static double area(double a, double b, double c) {
+        double p = ((a+b+c)/2);
         return Math.sqrt(p*(p-a)*(p-b)*(p-c));
     }
 
     public static void main(String[] args) {
-        double S = TrgArea.area(2,2,2,0);
+        double S = TrgArea.area(2,2,2);
         System.out.println(S);
     }
 }
