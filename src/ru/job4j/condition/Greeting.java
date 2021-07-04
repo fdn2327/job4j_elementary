84ab16ce1ff81b4910e4f944fe5db8b36a5994e8
fdn2327 <fdn2327@gmail.com>
2021-07-04 12:42:00 +0300

    Greeting_Исправлено

--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -4,10 +4,10 @@ public class Greeting {
     public static void main(String[] args) {
         String idea = "I like Java!";
         System.out.println(idea);
-        String idea2 = idea + " But I am newbie.";
+        idea = idea += " But I am newbie.";
         int year = 2021;
         String space = " ";
-        String idea3 = idea2 + space + year;
-        System.out.println(idea3);
+        idea += idea + space + year;
+        System.out.println(idea);
     }
 }
