21cbd6dd230173ba40a8f465c97a2afbcf1d1fd4
fdn2327 <fdn2327@gmail.com>
2021-07-07 22:13:22 +0300

    С применением CheckStyle

--- src/ru/job4j/condition/TrgArea.java
+++ src/ru/job4j/condition/TrgArea.java
@@ -3,12 +3,12 @@ package ru.job4j.condition;
 public class TrgArea {
 
     public static double area(double a, double b, double c) {
-        double p = ((a+b+c)/2);
-        return Math.sqrt(p*(p-a)*(p-b)*(p-c));
+        double p = ((a + b + c) / 2);
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
     }
 
     public static void main(String[] args) {
-        double S = TrgArea.area(2,2,2);
-        System.out.println(S);
+        double s = TrgArea.area(2, 2, 2);
+        System.out.println(s);
     }
 }
